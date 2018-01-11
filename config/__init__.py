720cc60e68146232db8996c91622d4cccb10bc0a
Nir Lavee <nir692007@gmail.com>
2018-01-11 20:24:24 +0200

    Add auxiliary time functionality.

--- config/__init__.py
+++ config/__init__.py
@@ -4,6 +4,7 @@
 Server configuration, constants and functionality.
 """
 
+from datetime import datetime
 import os
 import yaml
 
@@ -14,7 +15,19 @@ _CONFIG_FILE_PATH = os.path.join(_CONFIG_DIR, "config.yaml")
 with open(_CONFIG_FILE_PATH) as stream:
     _CONFIG = yaml.safe_load(stream)
 
-# Expose constants.
+# Expose paths.
 TASKS_DIR = _CONFIG["paths"]["tasks_dir"]
 CONTESTS_DIR = _CONFIG["paths"]["contests_dir"]
 USERS_FILE = _CONFIG["paths"]["users_file"]
+
+# Expose constants.
+TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+
+def time_from_str(time_str, str_format=TIME_FORMAT):
+    """
+    Convert the given string to a datetime object.
+    By default, accepts the time format constant above, e.g.:
+    "2000-01-01 10:00"
+    """
+    return datetime.strptime(time_str, str_format)
